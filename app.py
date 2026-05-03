145cc2c04107aef7503befb4595f86a184d32ec2
Saffhrin <saffhrinsaffh@gmail.com>
2026-05-03 11:00:16 +0530

    Update Flask app to use environment port and disable debug

--- app.py
+++ app.py
@@ -4114,5 +4114,6 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     flask_app = create_app()
-    flask_app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    flask_app.run(host="0.0.0.0", port=port, debug=False)
 
